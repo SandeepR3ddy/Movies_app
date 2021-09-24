3277f2876c9afedcdd62cf3f8226d1e9046e10ae
SandeepReddy Chinthakunta <sandeeprddy23@gmail.com>
2021-09-24 08:21:09 +0530

    completed delete funcitonality

---
+++ src/components/movies.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import {getMovies} from './movieService'
+
+export default class Movies extends Component {
+    constructor(props)
+        {
+            super(props);
+            this.state = {
+                movies:getMovies()
+            } 
+        }
+        handleDelete = (title) =>{
+         let newAr = this.state.movies.filter(Obj =>
+            {
+              return Obj.title != title;
+            })
+            this.setState({movies:newAr});
+        }
+    render() {
+        
+        return (
+            <div className = "container">
+            <div className = "row">
+                <div className = "col-3">
+                    <h1>Hello</h1>
+                </div>
+                <div className = "col-9">
+                    <input type = "text"></input>
+                <table className ="table">
+  <thead>
+    <tr>
+      <th scope="col">Title</th>
+      <th scope="col">Genre</th>
+      <th scope="col">Stock</th>
+      <th scope="col">Rate</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+        this.state.movies.map(movieObj =>(
+            <tr scope = "row" >
+                <td>{movieObj.title}</td>
+                <td>{movieObj.genre.name}</td>
+                <td>{movieObj.numberInStock}</td>
+                <td>{movieObj.dailyRentalRate}</td>
+                <td><button type="button" className ="btn btn-danger" onClick = {() => this.handleDelete(movieObj.title)}>Delete</button></td>                
+                </tr>
+        )
+        )
+    }
+  </tbody>
+</table>
+                </div>
+            </div>
+            </div>
+        )
+    }
+}
