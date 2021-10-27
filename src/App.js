413ce42b18fb93f649035d939dbc379bc8e7b51c
SandeepReddy Chinthakunta <sandeeprddy23@gmail.com>
2021-10-27 19:05:34 +0530

    route props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Movies from "./components/movies";
 import About from "./components/About";
 import Home from "./components/Home";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Nav from "./components/Nav";
 function App() {
   return (
     <Router>
+      <Nav/>
       <Switch>
+  <Route path = "/" exact component = {Home}/>
    <Route path = "/movies" component = {Movies}/>
-  <Route path = "/about" component = {About}/>
-  <Route path = "/" component = {Home}/>
+   <Route path='/about' render={(props)=>(
+      <About {...props} isAuth={true}/>
+    )}/>
    </Switch>
    </Router>
   )
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function About() {
+function About({isAuth}) {
+    console.log(isAuth);
     return (
         <div>
-            <h1>This is a About Component</h1>
+            <h1>This the about component</h1>
         </div>
     )
 }
+
+export default About
