7b4d293c739ec5f6f75fe92f336a609bb14d85a0
SandeepReddy Chinthakunta <sandeeprddy23@gmail.com>
2021-10-27 18:14:23 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Movies from "./components/movies";
 import About from "./components/About";
 import Home from "./components/Home";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 function App() {
   return (
-    <>
-    <Home/>
-   <Movies/>
-   <About/>
-   </>
+    <Router>
+      <Switch>
+   <Route path = "/movies" component = {Movies}/>
+  <Route path = "/about" component = {About}/>
+  <Route path = "/" component = {Home}/>
+   </Switch>
+   </Router>
   )
 }
 
